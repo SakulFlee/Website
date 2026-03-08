bf10b48cd9f485dbe3a7353e82432d1e0dc9e793
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-03-08 03:05:31 +0100

    Removed TailwindCSS

--- astro-blog/astro.config.mjs
+++ astro-blog/astro.config.mjs
@@ -3,13 +3,9 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sakul-flee.de",
   integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
